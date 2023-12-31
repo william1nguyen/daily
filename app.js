0e0edc02ed2204453bbb31de376a24356ab73e45
natalieconan <natalieconan17@gmail.com>
2023-12-31 09:32:23 +0700

    update out file for frontend

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 const { authorization } = require('./middlewares/authorization');
 
 var indexRouter = require('./routes/index');
@@ -12,16 +13,17 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'out')));
 
-app.use('/', indexRouter);
 app.use('/api', authorization, apiRouter);
 app.use('/users', usersRouter);
 
@@ -39,6 +41,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'out', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
