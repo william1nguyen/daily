933cf94f7d0247d571de923c48e172216b65c482
natalieconan <natalieconan17@gmail.com>
2023-12-31 17:10:06 +0700

    remove fullname from createuser

--- controllers/prisma.controller.js
+++ controllers/prisma.controller.js
@@ -2,7 +2,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const createUser = async (req) => {
-    const { fullname, username, email, hashedPassword } = req;
+    const { username, email, hashedPassword } = req;
     const user = await prisma.user.create({
         data: {
             username: username,
